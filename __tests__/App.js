debc91baac1a1f0aeea7be272fb3d4cd352fde8b
Altair343 <and97_26@hotmail.com>
2021-04-05 21:28:05 -0500

    configuración inicial par2

--- __tests__/App.js
+++ __tests__/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
+// Nota: el renderizador de prueba debe ser requerido después de react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
