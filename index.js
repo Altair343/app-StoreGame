e008036246a6e7501f78cf776cb6c6ca4c2d0c57
Altair343 <and97_26@hotmail.com>
2021-04-05 21:26:20 -0500

    configuración inicial

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import { registerRootComponent } from 'expo';
 
-import App from './App';
+import App from './src/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
+
+// registerRootComponent llama a AppRegistry.registerComponent ('main', () => App);
+// También asegura que si carga la aplicación en el cliente Expo o en una compilación nativa,
+// el entorno está configurado adecuadamente
+
 registerRootComponent(App);
