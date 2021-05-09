ae8f77cbdcc016361dde9107cd723fe016144895
Altair343 <and97_26@hotmail.com>
2021-05-09 11:38:17 -0500

    variables de estilos

---
+++ src/assets/styles/VarStyles.js
@@ -0,0 +1,21 @@
+const VarStyles = {
+    color: {
+        primaryColor: '#ed3b3b',
+        secondaryColor: '#AC010A',
+    },
+    grey: {
+        white: '#FFFFFF',
+        G100: '#f8f9fa',
+        G200: '#e9ecef',
+        G300: '#dee2e6',
+        G400: '#ced4da',
+        G500: '#adb5bd',
+        G600: '#6c757d',
+        G700: '#495057',
+        G800: '#343a40',
+        G900: '#212529',
+        black: '#000000',
+    }
+}
+
+export default VarStyles;
