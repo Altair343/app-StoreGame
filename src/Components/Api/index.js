0c43a70edf0e68edb8a4bc6c4fbb9cd0c24feb14
Altair343 <and97_26@hotmail.com>
2021-05-09 11:38:45 -0500

    rutas de la api

---
+++ src/Components/Api/index.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+
+const urlBase = 'http://192.168.1.65:4000/api';
+
+/* const config = {
+    headers: {
+        Authorization: `Bearer ${userLogged}`
+    }
+} */
+
+/* {
+    "username": "Mei",
+    "email": "mei@api.com",
+    "password": "12345678"
+} */
+
+const Singnin = async (email, password) => {
+    try {
+        const credentials = {
+            email,
+            password
+        }
+        const { data } = await axios.post(`${urlBase}/auth/signin`, credentials)
+        return data;
+    } catch (error) {
+        return error.response.data;
+    }
+}
+
+const Singup = async (newUser) => {
+    try {
+        const { data } = await axios.post(`${urlBase}/auth/signup`, newUser)
+        return data;
+    } catch (error) {
+        return error.response.data;
+    }
+}
+
+const Sales = async (sale) => {
+    try {
+        const { data } = await axios.get(`${urlBase}/products/sales/${sale}`)
+        return data;
+    } catch (error) {
+        return error.response.data;
+    }
+}
+
+export default { Singnin, Singup, Sales };
