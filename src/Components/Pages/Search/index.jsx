28294d48e3ef78adf78bc1e7b7932954cfee68b8
Altair343 <and97_26@hotmail.com>
2021-05-10 21:18:56 -0500

    creando plantilla de la vista de todos los juegos

---
+++ src/Components/Pages/Search/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+// Styles
+import { styles } from './styles';
+
+const Search = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Busqueda</Text>
+        </View>
+    )
+}
+
+export default Search;
