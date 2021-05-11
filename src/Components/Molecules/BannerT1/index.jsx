3003d88fc4c4b61a90e31e38b85a978b5ead18bc
Altair343 <and97_26@hotmail.com>
2021-05-10 21:17:24 -0500

    agregando la plantilla den banner 1

---
+++ src/Components/Molecules/BannerT1/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Image,Text } from 'react-native';
+
+// Styles
+import { styles } from './styles';
+
+const BannerT1 = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Banner</Text>
+        </View>
+    )
+}
+
+export default BannerT1;
